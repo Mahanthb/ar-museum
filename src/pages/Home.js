fd6bfd3f9167020e6df2151fe1b2b302055b932e
Mahanthb <142413322+Mahanthb@users.noreply.github.com>
2024-11-05 11:20:34 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,17 +17,17 @@ function Home() {
       <div className="card-container">
         <div className="card">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb8_ZOHNdBUXI8N0EU7cZkfLRdjdwLzDmQ3w&s" alt="Exhibit 1" />
-          <h3>Exhibit 1</h3>
+          <h3>Explore</h3>
           <p>Explore the stunning details of ancient artifacts.</p>
         </div>
         <div className="card">
           <img src="https://img.jagranjosh.com/images/2023/October/4102023/Expensive-Paintings.jpg" alt="Exhibit 2" />
-          <h3>Exhibit 2</h3>
+          <h3>Discover</h3>
           <p>Discover the history behind famous paintings.</p>
         </div>
         <div className="card">
           <img src="https://plus.unsplash.com/premium_photo-1664438942574-e56510dc5ce5?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YWJzdHJhY3QlMjBhcnR8ZW58MHx8MHx8fDA%3D" alt="Exhibit 3" />
-          <h3>Exhibit 3</h3>
+          <h3>Exhibit</h3>
           <p>Immerse yourself in the beauty of modern art.</p>
         </div>
       </div>
